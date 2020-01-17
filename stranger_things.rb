f3b53d15da37262b3e19e81e53c0b7514fc70f1b
jmeza2003 <44911970+jmeza2003@users.noreply.github.com>
2020-01-17 14:47:41 -0800

    Add files via upload

---
+++ stranger_things.rb
@@ -0,0 +1,19 @@
+# Welcome to Sonic Pi v3.
+live_loop :my_loop do
+  play:c4, rate: 0.3
+  sleep 0.5
+  play:e4, rate: 0.3
+  sleep 0.5
+  play:g4, rate: 0.3
+  sleep 0.5
+  play:b4, rate: 0.3
+  sleep 0.5
+  play:c5, rate: 0.3
+  sleep 0.5
+  play:b4, rate: 0.3
+  sleep 0.5
+  play:g4, rate: 0.3
+  sleep 0.5
+  play:c4, rate: 0.3
+  sleep 0.5
+end
